1638a9e135e3d82ac4e5a78eed72c2c8e8d8277d
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-12-10 19:05:15 +0100

    Add firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyCbKXXyKVffYXnuXgBNStsax87dWxSK0kc",
+  authDomain: "fir-9-cc.firebaseapp.com",
+  projectId: "fir-9-cc",
+  storageBucket: "fir-9-cc.appspot.com",
+  messagingSenderId: "1063614345406",
+  appId: "1:1063614345406:web:ecfeb9f85667794c8eaeef"
 };
 
+// Initialize Firebase
 initializeApp(firebaseConfig);
+
+// Initialize services
+const db = getFirestore();
+
+// Collection references
+const colRef = collection(db, 'books');
+
+// Get collection data
+getDocs(colRef)
+  .then(snaphshot => {
+    let books = [];
+    snaphshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch(err => {
+    console.error(err.meesage);
+  });
